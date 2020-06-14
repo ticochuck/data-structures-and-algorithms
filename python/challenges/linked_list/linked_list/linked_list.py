72d99cf54d56c2c5efc9b05e98683cff808e6f54
ticochuck <chucklivilla@gmail.com>
2020-06-13 17:56:05 -0700

    able to insert to the linkedlist and print the head and next values

--- python/challenges/linked_list/linked_list/linked_list.py
+++ python/challenges/linked_list/linked_list/linked_list.py
@@ -1,36 +1,70 @@
 class Node:
     """
-    [summary]
+    This is the Node class
     """
 
-    def __init__(self, value, next_node):
-        """[summary]
+    def __init__(self, value, next_node = None):
+        """
+        This is to create a node with a value and a next reference
 
         Args:
-            value ([type]): [description]
-            next_node ([type]): [description]
+            value : node value 
+            next_node : reference to next node
         """
         
         self.value = value
         self.next_node = next_node
 
     def __repr__(self):
-        pass
+        return {'Value':{self.value}, 'Next Node':{self.next_node}}
+        
 
     def __str__(self):
-        pass
+        return f"{self.value}, Next_Node={self.next_node}"
 
 
 class LinkedList:
     """
-    [summary]
+    This is the class LinkedList
     """
     def __init__(self):
+        """
+        This is to initializa the LinkedList
+        """
 
         self.head = None
-    
-    def insert(self, value = None):
-        pass
 
-    def to_string(self):
-        pass
+    
+    def __str__(self):
+        """
+        prints LinkedList 
+        """
+        return f"head: {self.head}"
+
+    
+    def insert(self, value):
+        """
+        Method to insert a node to the LinkedList.
+        Value is set to none for the fisrt node of the LinkedList
+        """
+        
+        node = Node(value)
+        
+        if self.head is not None:
+            node.next_node = self.head
+        self.head = node
+
+
+
+ll = LinkedList()
+#print(ll)
+
+ll.insert('Saturday')
+ll.insert('Friday')
+ll.insert('Thursday')
+ll.insert('Wednesday')
+ll.insert('Tuesday')
+ll.insert('Monday')
+ll.insert('Sunday')
+
+print(ll.head)
