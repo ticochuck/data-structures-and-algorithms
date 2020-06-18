55994783c18ea9737772b97d349e030cd34a8bf9
ticochuck <chucklivilla@gmail.com>
2020-06-17 17:11:59 -0700

    completed challenge 07, fixed method, added whiteboard to readme, added tests

--- python/challenges/linked_list/linked_list/linked_list.py
+++ python/challenges/linked_list/linked_list/linked_list.py
@@ -105,7 +105,7 @@ class LinkedList:
                 current.next_node = node
                 return
             else:
-                return print(f'Node with value of {value} does not exist')
+                return (f'Node with value of {value} does not exist')
             current = current.next_node
         
 
@@ -128,57 +128,49 @@ class LinkedList:
                 current.next_node = node
                 return
             if current.next_node == None:
-                return print(f'Node with value of {value} does not exist')
+                return (f'Node with value of {value} does not exist')
             current = current.next_node
     
 
-    def kth_from_end(self, k):
+    def kth_value(self, k):
         """
         Returns the nth value of the Linkedlist starting from the end
         """
         current = self.head
         count = 0
-        while current.next_node is not None:
+        while current:
             count += 1
             current = current.next_node
+        
         index = count - k
-        count2 = 0
+        
+        count2 = 1
         current = self.head
         if index < 0:
-            return 'Exemption'
+            return 'Exception'
         else:
-            while current.next_node is not None:
+            while current:
                 if count2 == index:
-                    return print(current.value)
+                    return current.value
                 count2 += 1
                 current = current.next_node
+
             
 
 
 ll = LinkedList()
 ll.insert(2)
+ll.insert(8)
 ll.insert(3)
 ll.insert(1)
 
 
-# ll.insert('Saturday')
-# ll.insert('Friday')
-# ll.insert('Thursday')
-# ll.insert('Wednesday')
-# ll.insert('Tuesday')
-# ll.insert('Monday')
-# ll.insert('Sunday')
-
-# ll.append('March')
-# ll.append('February')
-# ll.append('January')
-
-# ll.insert_before('March', 'Hola')
-ll.insert_before(3,5)
-#ll.insert_after(4,5)
+#ll.insert_before(4,5)
+ll.insert_after(4,5)
+print(ll.kth_value(6))
 print(ll.head)
 print(ll.includes(3))
 print(ll.includes('November'))
 
-#print(Node(ll))
+
 #print(ll.kth_from_end(0))
