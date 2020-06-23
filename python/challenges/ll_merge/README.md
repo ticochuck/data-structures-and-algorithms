1db4fa512a4e1392e5bb8b645add1deca12e9fd1
ticochuck <chucklivilla@gmail.com>
2020-06-22 21:17:52 -0700

    initial stuff

---
+++ python/challenges/ll_merge/README.md
@@ -0,0 +1,11 @@
+### Code Challenge #8
+Write a function called mergeLists which takes two linked lists as arguments. Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list. Try and keep additional space down to O(1). You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+
+### White Board
+![](../../challenges/assets/ccmerge.png)
+
+### Tests
+ll1 = 1, 3, 2
+ll2 = 5, 9, 4
+
+output: 1, 5, 3, 9, 2, 4
---
+++ python/challenges/ll_merge/ll_merge/ll_merge.py
@@ -0,0 +1,23 @@
+@staticmethod
+    def merge_lists(ll1, ll2):
+        
+
+        if not ll1.head:
+            return ll2.head
+        elif not ll2.head:
+            return ll1.head
+        
+        current1, current2 = ll1.head, ll2.head
+        while current1:
+            if current2:
+                tmp1, current1.next_node = current1.next_node, current2
+                if tmp1:
+                    tmp2, current2.next_node = current2.next_node, tmp1
+                if not tmp2:
+                    break
+                
+                current1, current2 = tmp1, tmp2
+        return ll1.head
+
+
+print(LinkedList.merge_lists([1,3,2], [5,9,4]))
