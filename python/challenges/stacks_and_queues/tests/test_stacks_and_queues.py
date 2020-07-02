246aa4bae582e52797c926ac97a2308d8ccac392
ticochuck <chucklivilla@gmail.com>
2020-07-02 14:25:22 -0700

    added poetry dependancies and initial test

--- python/challenges/stacks_and_queues/tests/test_stacks_and_queues.py
+++ python/challenges/stacks_and_queues/tests/test_stacks_and_queues.py
@@ -1,5 +1,14 @@
 from stacks_and_queues import __version__
-
+from stacks_and_queues.stacks_and_queues import Node, Stack
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_node_exist():
+    assert Node
+
+
+def test_Stack_exist():
+    assert Stack
+
