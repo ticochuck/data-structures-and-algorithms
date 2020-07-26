2a81dfdbf8ea50945e15ca125145dd1f389d7a6d
ticochuck <chucklivilla@gmail.com>
2020-07-25 18:13:46 -0700

    initial files

---
+++ python/challenges/tree/tree/tree.py
@@ -0,0 +1,23 @@
+class Node:
+    """
+    This is the Node class. 
+    """
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def pre_order(self):
+        pass
+
+    def in_order(self):
+        pass
+
+    def post_order(self):
+        pass
+
+
